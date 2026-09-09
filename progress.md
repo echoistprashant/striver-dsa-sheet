796784b5158867da0399ed5d71686aca20b4406d
echoishtprashant <yadavprashant100804@gmail.com>
2026-09-10 03:58:58 +0530

    sort character by frequency

--- progress.md
+++ progress.md
@@ -129,7 +129,8 @@
 - [X] largest odd number in string 
 - [X] Longest Common Prefix
 - [X] rotate string
-- [X] rotate string
+- [X] valid anagram 
+- [X] sort character by frequency
 
 
 
---
+++ string/sortfrequency.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+
+        for(char c : s) {
+            mp[c]++;
+        }
+
+        vector<pair<char, int>> freq(mp.begin(), mp.end());
+
+        sort(freq.begin(), freq.end(), [](auto &a, auto &b) {
+            return a.second > b.second;
+        });
+
+        string ans = "";
+
+        for(auto &p : freq) {
+            ans += string(p.second, p.first);
+        }
+
+        return ans;
+    }
+};
